367b7c4c7918c10267ae96fd975cbc1190c5a990
Tobias Hofmann <tobias@plural.com>
2025-05-06 16:06:38 -0700

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,6 +1,7 @@
 print("Hello World")
 
-oper1 = 1
-oper2 = 2
-i = oper1+oper2
-print("The sum of {}+{} is {}".format(oper1,oper2,i))
+def calc(oper1, oper2):
+  i = oper1+oper2
+  print("The sum of {}+{} is {}".format(oper1,oper2,i))
+
+calc(1,2)
